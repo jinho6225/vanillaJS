b5885e2877c9b05db3729418bb17e8b1f616f290
jinho6225 <jinho6225@hotmail.com>
2020-01-23 20:57:25 -0800

    create getTime function

---
+++ main.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector('.js-clock'),
+  clock = clockContainer.querySelector('h1');
+
+const date = new Date()
+
+
+function getTime() {
+  const date = new Date()
+  const hr = date.getHours()
+  const min = date.getMinutes()
+  const sec = date.getSeconds()
+  clock.innerHTML = `${hr < 10 ? `0${hr}` : hr}:${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}`
+}
+
+
+function init() {
+  setInterval(getTime, 1000)
+}
+
+init()
